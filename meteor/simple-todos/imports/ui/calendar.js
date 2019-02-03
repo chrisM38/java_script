538287b25c0daabb63b955617e88bd285706aab0
chrisM38 <chris.m3817@gmail.com>
2019-02-03 19:05:15 +0100

    calendar

--- meteor/simple-todos/imports/ui/calendar.js
+++ meteor/simple-todos/imports/ui/calendar.js
@@ -3,17 +3,16 @@ import {Template} from 'meteor/templating';
 import './calendar.html';
 import './body.html';
 
-import { today,currentYear,currentMonth,months} from '../api/dates.js';
+import { currentYear,currentMonth,months} from '../api/dates.js';
 
 
 Template.calendar.helpers({
     setYear() {
         return currentYear;
-
     },
     setMonth() {
         return months[currentMonth];
-    }
+    },
 });
 
 Template.calendar.events({
@@ -38,40 +37,25 @@ Template.calendar.onRendered(function () {
 function showCalendar(month, year) {
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
     let tbl = document.getElementsByClassName("days");
-
     tbl[0].innerHTML = "";
-
-    // creating all cells
     let date = 1;
     for (let i = 0; i < 6; i++) {
-        // creates a table row
         let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
         for (let j = 0; j < 7; j++) {
             if (i === 0 && j < firstDay -1) {
                 let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
+                cell.appendChild(document.createTextNode(""));
                 row.appendChild(cell);
             } else if (date > daysInMonth) {
                 break;
             } else {
                 let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
+                cell.appendChild(document.createTextNode(date));
                 row.appendChild(cell);
                 date++;
             }
-
-
         }
-
-        tbl[0].appendChild(row); // appending each row into calendar body.
+        tbl[0].appendChild(row);
     }
 }
