591e0ac16429ef34fd5ec1a500a7df24752abbe9
chrisM38 <chris.m3817@gmail.com>
2019-02-07 13:52:38 +0100

    abc

--- meteor/simple-todos/imports/ui/body.js
+++ meteor/simple-todos/imports/ui/body.js
@@ -1,31 +1,31 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
+import {Meteor} from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+import {ReactiveDict} from 'meteor/reactive-dict';
 
 
-import { Tasks } from '../api/tasks.js';
+import {Tasks} from '../api/tasks.js';
 import {getMonth, getSelectedDays, getYear} from "./calendar";
 
 import './calendar.js';
 import './task.js';
 import './body.html';
 
+const defaultPriority = "Low";
 
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
-    console.log(this.state);
 });
 
 Template.body.helpers({
     tasks() {
         const instance = Template.instance();
         if (instance.state.get('hideCompleted')) {
-            return Tasks.find({ checked: { $ne: true }, month: getMonth() + 1, year:getYear()}, { sort: { day: 1 } }).fetch();
+            return Tasks.find({checked: {$ne: true}, month: getMonth() + 1, year: getYear()}, {sort: {day: 1}}).fetch();
         }
-        return Tasks.find({month: getMonth() + 1, year:getYear()}, {sort: {day: 1}}).fetch();
+        return Tasks.find({month: getMonth() + 1, year: getYear()}, {sort: {day: 1}}).fetch();
     },
     incompleteCount() {
-        return Tasks.find({ checked: { $ne: true } }).count();
+        return Tasks.find({checked: {$ne: true}}).count();
     },
 
 });
@@ -35,19 +35,27 @@ Template.body.events({
         event.preventDefault();
 
         const target = event.target;
+        const priority = target.priority.value;
         const text = target.text.value;
 
         let month = getMonth() + 1;
         let year = getYear();
         let days = getSelectedDays();
 
-        if (days.length !== 0){
-        for(let day of days) {
-            Tasks.insert({
-                text,
-                day, month, year,
-            });
-        }}else {throw new Meteor.Error('bad',"You have no selected days");}
+        if(priority !== ""){
+            if (days.length !== 0) {
+                for (let day of days) {
+                    Tasks.insert({
+                        text,
+                        day, month, year, priority
+                    });
+                }
+            } else {
+                alert("You have no selected days");
+            }
+        } else {
+            alert("You have no selected priority");
+        }
 
         target.text.value = '';
     },
@@ -55,9 +63,14 @@ Template.body.events({
         console.log(event.target.checked);
         instance.state.set('hideCompleted', event.target.checked);
     },
-    'click .refresh'(event, instance){
-        instance.state.set('hideCompleted', true);
-        instance.state.set('hideCompleted', false);
+    'click .refresh'(event, instance) {
+        if (instance.state.get('hideCompleted')) {
+            instance.state.set('hideCompleted', false);
+            instance.state.set('hideCompleted', true);
+        } else {
+            instance.state.set('hideCompleted', true);
+            instance.state.set('hideCompleted', false);
+        }
     }
 });
 
--- meteor/simple-todos/imports/ui/calendar.js
+++ meteor/simple-todos/imports/ui/calendar.js
@@ -13,7 +13,6 @@ Template.calendar.helpers({
         return currentYear;
     },
     setMonth() {
-        console.log("test");
         return months[currentMonth];
     },
 });
